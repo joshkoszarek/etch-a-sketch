d66e25e108902fbff9ff4df0b2f631d8a0f10840
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com.>
2024-05-21 15:52:12 -0500

    Add ability to change pen color

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('hello there'); 
+
 
 //let userInputForGrid = Number(prompt('Please enter a number between 1 and 100:'));
 let userInputForGrid = 16;
@@ -12,8 +12,6 @@ for (let i = 0; i < (userInputForGrid **2); i++){
 
 }
 
-
-
 let gridHoveredOver = document.querySelectorAll(".grid-element"); 
 gridHoveredOver.forEach((gridBox) => { 
     gridBox.addEventListener("mouseover", (e) =>{ 
@@ -21,14 +19,47 @@ gridHoveredOver.forEach((gridBox) => {
     });
 });
 
-/*
-gridHoveredOver.addEventListener("mouseover", () => { 
-    console.log(); 
-});
-*/ 
 
 /*
-gridHoveredOver.addEventListener("click", (e) => { 
-    e.target.style.backgroundColor = 'blue';
-})
+Pen Button To Change Color
 */
+const orange_btn = document.querySelector('#second-color');
+const green_btn = document.querySelector('#third-color');
+const blue_btn = document.querySelector('#first-color');
+blue_btn.addEventListener('click', () => { 
+    gridHoveredOver.forEach((gridBox) => { 
+        gridBox.classList.remove("orange");
+        gridBox.classList.remove("green");
+        gridBox.classList.add("blue");
+       
+
+    })
+    blue_btn.classList.add('highlight-selection');
+    green_btn.classList.remove('highlight-selection');
+    orange_btn.classList.remove('highlight-selection');
+})
+
+
+green_btn.addEventListener('click', () => { 
+    gridHoveredOver.forEach((gridBox) => { 
+        gridBox.classList.remove("orange");
+        gridBox.classList.remove("blue");
+        gridBox.classList.add("green");
+    })
+    blue_btn.classList.remove('highlight-selection');
+    green_btn.classList.add('highlight-selection');
+    orange_btn.classList.remove('highlight-selection');
+})
+
+
+orange_btn.addEventListener('click', () => { 
+    gridHoveredOver.forEach((gridBox) => { 
+        gridBox.classList.remove("blue");
+        gridBox.classList.remove("green");
+        gridBox.classList.add("orange");
+    })
+    blue_btn.classList.remove('highlight-selection');
+    green_btn.classList.remove('highlight-selection');
+    orange_btn.classList.add('highlight-selection');
+})
+
