8a82a82d73760e7387fb1aec916de310f3a470d9
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com.>
2024-05-21 16:34:52 -0500

    Add background color selector and functionality

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+const orange_btn = document.querySelector('#second-color');
+const green_btn = document.querySelector('#third-color');
+const blue_btn = document.querySelector('#first-color');
 
+const black_background_color_btn = document.querySelector("#black");
+const white_background_color_btn = document.querySelector("#white");
+const container = document.querySelector('.main-container')
 
 //let userInputForGrid = Number(prompt('Please enter a number between 1 and 100:'));
 let userInputForGrid = 16;
-const container = document.querySelector('.main-container')
+
 for (let i = 0; i < (userInputForGrid **2); i++){ 
     let newGridItem = document.createElement('div');
     newGridItem.setAttribute("class", "grid-element"); 
+   
     let gridElementSize = 640/userInputForGrid;
     newGridItem.setAttribute("style", `height: ${gridElementSize}px; width: ${gridElementSize}px;`);
     container.appendChild(newGridItem); 
@@ -23,9 +30,16 @@ gridHoveredOver.forEach((gridBox) => {
 /*
 Pen Button To Change Color
 */
-const orange_btn = document.querySelector('#second-color');
-const green_btn = document.querySelector('#third-color');
-const blue_btn = document.querySelector('#first-color');
+/*
+gridHoveredOver.forEach((gridBox) => { 
+    gridBox.classList.add('blue');     
+    gridBox.classList.add('white');
+} )
+
+blue_btn.classList.add('highlight-selection');
+white_background_color_btn.classList('highlight-selection');
+*/
+
 blue_btn.addEventListener('click', () => { 
     gridHoveredOver.forEach((gridBox) => { 
         gridBox.classList.remove("orange");
@@ -63,3 +77,25 @@ orange_btn.addEventListener('click', () => {
     orange_btn.classList.add('highlight-selection');
 })
 
+/* 
+Change background color: 
+*/
+
+
+black_background_color_btn.addEventListener('click', () => { 
+    gridHoveredOver.forEach((gridBox) => {  
+        gridBox.classList.remove('white');
+        gridBox.classList.add('black'); 
+    })
+    black_background_color_btn.classList.add('highlight-selection'); 
+    white_background_color_btn.classList.remove('highlight-selection');
+})
+
+white_background_color_btn.addEventListener('click', () => { 
+    gridHoveredOver.forEach((gridBox) => {  
+        gridBox.classList.remove('black');
+        gridBox.classList.add('white'); 
+    })
+    black_background_color_btn.classList.remove('highlight-selection'); 
+    white_background_color_btn.classList.add('highlight-selection');
+})
