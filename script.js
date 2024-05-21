5b28adbe66a499deccfe9bbbae884d4c21c846a1
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com.>
2024-05-21 16:54:58 -0500

    Add reset button functionality

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const black_background_color_btn = document.querySelector("#black");
 const white_background_color_btn = document.querySelector("#white");
 const container = document.querySelector('.main-container')
 
+const reset_btn = document.querySelector("#reset-btn"); 
+
 //let userInputForGrid = Number(prompt('Please enter a number between 1 and 100:'));
 let userInputForGrid = 16;
 
@@ -98,4 +100,14 @@ white_background_color_btn.addEventListener('click', () => {
     })
     black_background_color_btn.classList.remove('highlight-selection'); 
     white_background_color_btn.classList.add('highlight-selection');
+})
+
+reset_btn.addEventListener('click', () => { 
+    gridHoveredOver.forEach((gridBox) => { 
+        gridBox.classList.remove("draw", "orange","green", "blue");
+        gridBox.classList.add("blue"); 
+    })
+    blue_btn.classList.add("highlight-selection");
+    orange_btn.classList.remove("highlight-selection");
+    green_btn.classList.remove("highlight-selection");
 })
